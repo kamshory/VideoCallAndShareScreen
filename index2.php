4ce279322a53624ec782d564c03ad1523998e5b9
Kamshory, MT <kamshory@alto.id>
2024-08-23 07:23:12 +0700

    Update

--- index2.php
+++ index2.php
@@ -5,21 +5,66 @@ $_SESSION['websocketChannel'] = 'weiuewiufweg';
 $_SESSION['websocketChannelTarget'] = 'weiuewiufwef';
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video Call and Screen Share</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <h1>Video Call and Screen Share</h1>
     <div id="video-container">
-        <video id="localVideo" autoplay playsinline></video>
+        <video id="localVideo" autoplay playsinline muted></video>
         <video id="remoteVideo" autoplay playsinline></video>
     </div>
-    <button id="openCamera">Open Camera</button>
-    <button id="startCall">Start Call</button>
-    <button id="shareScreen">Share Screen</button>
+    <button class="btn btn-primary" id="openCamera">Open Camera</button>
+    <button class="btn btn-primary" id="startCall">Start Call</button>
+    <button class="btn btn-primary" id="answer">Answer</button>
+    <button class="btn btn-primary" id="shareScreen">Share Screen</button>
     <script src="script.js"></script>
+
+    <div class="modal fade" id="incomming-video-call-dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Video Call</h5>
+                </div>
+                <div class="modal-body">
+                    Incomming video call
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary accept-call">Accept</button>
+                    <button type="button" class="btn btn-secondary reject-call">Reject</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="general-dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Title</h5>
+                </div>
+                <div class="modal-body">
+                    Body
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary close-modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="toast-wrapper">
+        
+    </div>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const openCamera = document.getElementById('openCamera');
-const startCallButton = document.getElementById('startCall');
-const shareScreenButton = document.getElementById('shareScreen');
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
-const userIdInput = document.getElementById('userId');
-const websocketUrl = 'ws://localhost:8080/path?param=value';
-
+let openCamera = document.getElementById('openCamera');
+let startCallButton = document.getElementById('startCall');
+let answerButton = document.getElementById('answer');
+let shareScreenButton = document.getElementById('shareScreen');
+let localVideo = document.getElementById('localVideo');
+let remoteVideo = document.getElementById('remoteVideo');
+let userIdInput = document.getElementById('userId');
+let protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+let port = window.location.port === "https:" ? "" : ":8080";
+let host = window.location.host;
+let websocketUrl = `${protocol}//${host}${port}/path/to/websocket?param=value`;
 let reconnectInterval = 3000;
 let localStream;
 let remoteStream;
 let peerConnection;
 let socket;
+let sendCall = false;
+let acceptCall = false;
+let receiveAnswer = false; 
+let receiveReject = false; 
+let waitingAnswerTime = 10000; // 10 seconds
 
-const servers = {
+let servers = {
     iceServers: [
         {
             urls: 'stun:stun.l.google.com:19302'
@@ -21,11 +29,12 @@ const servers = {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    
     initializeWebSocket();
 });
 
+/**
+ * Initialize websocket
+ */
 function initializeWebSocket()
 {
     socket = new WebSocket(websocketUrl);
@@ -35,17 +44,17 @@ function initializeWebSocket()
     
     socket.onmessage = async event => {
         console.log(event.data)
-        const message = JSON.parse(event.data);
+        let message = JSON.parse(event.data);
         processIncommingMessage(message);
     };
     
     socket.addEventListener('close', function(event) {
-        console.log('WebSocket terputus:', event);
+        // reconnect websocket when closed
         setTimeout(initializeWebSocket, reconnectInterval);
     });
 
     socket.addEventListener('error', function(event) {
-        console.error('WebSocket error:', event);
+        // close websocket when error occured
         socket.close();
     });
 }
@@ -55,13 +64,13 @@ openCamera.addEventListener('click', async () => {
     localVideo.srcObject = localStream;
 
     // Create an AudioContext to process the audio
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const audioSource = audioContext.createMediaStreamSource(localStream);
-    const audioDestination = audioContext.createMediaStreamDestination();
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    let audioSource = audioContext.createMediaStreamSource(localStream);
+    let audioDestination = audioContext.createMediaStreamDestination();
     audioSource.connect(audioDestination);
 
     // Create a new stream with the processed audio
-    const processedStream = new MediaStream([...localStream.getVideoTracks(), ...audioDestination.stream.getAudioTracks()]);
+    let processedStream = new MediaStream([...localStream.getVideoTracks(), ...audioDestination.stream.getAudioTracks()]);
 
     peerConnection = new RTCPeerConnection(servers);
     peerConnection.onicecandidate = event => {
@@ -81,7 +90,14 @@ openCamera.addEventListener('click', async () => {
 });
 
 startCallButton.addEventListener('click', async () => {
-    const offer = await peerConnection.createOffer();
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.send(constructMessageVideoCall());
+    socket.send(constructMessageOffer(offer));
+});
+
+answerButton.addEventListener('click', async () => {
+    let offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
     socket.send(constructMessageOffer(offer));
 });
@@ -96,16 +112,165 @@ function processIncommingMessage(message)
 
 async function processVideoCall(message)
 {
-    if (message.type === 'offer') {
+    if (message.type === 'video-call') {
+        // menerima sinyal pangillan masuk
+        onReceiveVideoCall();
+    }
+    if (message.type === 'missed-call') {
+        // menerima sinyal pangillan tak terjawab dari orang yang menghubungi
+        onReceiveMissedCall();
+    }
+    if (message.type === 'reject-call') {
+        // menerima sinyal penolakan dari orang yang dihubungi
+        onReceiveCallRejection();
+    }
+    else if (message.type === 'offer') {
+        // menerima sinyal offer
         await peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
-        const answer = await peerConnection.createAnswer();
+        let answer = await peerConnection.createAnswer();
         await peerConnection.setLocalDescription(answer);
         socket.send(constructMessageAnswer(answer));
     } else if (message.type === 'answer') {
+        // menerima sinyal answer
         await peerConnection.setRemoteDescription(new RTCSessionDescription(message.answer));
     } else if (message.type === 'candidate') {
+        // menerima sinyal candidate
         await peerConnection.addIceCandidate(new RTCIceCandidate(message.candidate));
     }
+}
+
+function showToast(message, title, smallText, iconUrl, delay, container)
+{
+    iconUrl = iconUrl || '';
+    delay = delay || 5000;
+    container = container || '.toast-wrapper';
+    let id = (new Date()).getTime();
+    let template = `<div id="${id}" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+        <div class="toast-header">
+            <img src="${iconUrl}" class="rounded mr-2 toast-icon" alt="...">
+            <strong class="mr-auto">${title}</strong>
+            <small class="text-muted">${smallText}</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            ${message}
+        </div>
+    </div>`;
+    
+    let toast = $(template);
+    if(iconUrl == ''){
+        toast.find('.toast-icon').remove();
+    }
+    toast.prependTo(container).toast('show');
+    setTimeout(function(){
+        toast.remove();
+    }, delay);
+}
+
+function onReceiveVideoCall()
+{
+    $('#incomming-video-call-dialog').modal('show');
+}
+
+function onReceiveMissedCall()
+{
+    $('#incomming-video-call-dialog').modal('hide');
+    showToast(
+        'Anda melewatkan panggilan video hingga waktu tunggu habis', 
+        'Panggilan Anda Tidak Dijawab', 
+        'Saat ini juga'
+    );
+}
+
+function onMissedCall()
+{
+    showToast(
+        'Panggilan Anda tidak dijawab hingga waktu tunggu habis', 
+        'Panggilan Anda Tidak Dijawab', 
+        'Saat ini juga'
+    );
+}
+
+function onReceiveCallRejection()
+{
+    sendCall = false;
+    acceptCall = false;
+    receiveAnswer = false; 
+    receiveReject = false; 
+    receiveReject = true;
+}
+
+function onReceiveAnswer()
+{
+    sendCall = false;
+    acceptCall = false;
+    receiveAnswer = true; 
+    receiveReject = false; 
+    receiveReject = true;
+}
+
+function sendVideoCallCancellation()
+{
+    sendCall = false;
+    acceptCall = false;
+    receiveAnswer = false; 
+    receiveReject = false; 
+    receiveReject = false;
+    socket.send(constructMessageMissedCall());
+}
+
+function checkReceiveAnswer()
+{
+    if(sendCall && !receiveAnswer)
+    {
+        onMissedCall();
+        sendVideoCallCancellation();
+    }
+}
+
+function sendVideoCallRequest()
+{
+    sendCall = true;
+    acceptCall = false;
+    receiveAnswer = false; 
+    setTimeout(function(){
+        checkReceiveAnswer();
+    }, waitingAnswerTime);
+}
+
+function constructMessageMissedCall()
+{
+    return JSON.stringify({ 
+        feature: 'video-call',
+        type: 'missed-call'
+    });
+}
+
+function constructMessageVideoCall()
+{
+    sendVideoCallRequest();
+    return JSON.stringify({ 
+        feature: 'video-call',
+        type: 'video-call'
+    });
+}
+
+function constructMessageRejectCall()
+{
+    return JSON.stringify({ 
+        feature: 'video-call',
+        type: 'reject-call'
+    });
+}
+
+function constructMessageEndCall()
+{
+    return JSON.stringify({ 
+        feature: 'video-call',
+        type: 'end-call'
+    });
 }
 
 function constructMessageOffer(offer)
@@ -135,11 +300,10 @@ function constructMessageAnswer(answer)
     });
 }
 
-
 shareScreenButton.addEventListener('click', async () => {
-    const screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-    const screenTrack = screenStream.getVideoTracks()[0];
-    const sender = peerConnection.getSenders().find(s => s.track.kind === 'video');
+    let screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+    let screenTrack = screenStream.getVideoTracks()[0];
+    let sender = peerConnection.getSenders().find(s => s.track.kind === 'video');
     sender.replaceTrack(screenTrack);
     screenTrack.onended = () => {
         sender.replaceTrack(localStream.getVideoTracks()[0]);
